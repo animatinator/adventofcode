38cd3208825cf04166c1ec7ebdf67a16f08d4dd6
David Barker <animatinator@gmail.com>
2020-12-04 21:48:05 +0000

    Use C++17 features

--- 2020-01/2020-01b.cc
+++ 2020-01/2020-01b.cc
@@ -2,6 +2,7 @@
 #include <exception>
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <string>
 #include <utility>
 #include <vector>
@@ -22,8 +23,7 @@ std::vector<int> ParseNumberListFile(std::string file_path) {
     return result;
 }
 
-// TODO: Update compiler and use std::optional.
-std::pair<std::pair<int, int>, bool> IndicesWithSumInSubrange(std::vector<int> numbers, int target, int l, int r) {
+std::optional<std::pair<int, int>> IndicesWithSumInSubrange(std::vector<int> numbers, int target, int l, int r) {
     for (int sum = 0; sum != target; sum = numbers[l] + numbers[r]) {
         if (sum > target) {
             r--;
@@ -32,11 +32,11 @@ std::pair<std::pair<int, int>, bool> IndicesWithSumInSubrange(std::vector<int> n
         }
         // Check the indexes haven't crossed.
         if (l >= r) {
-            return {{0, 0}, false};
+            return {};
         }
     }
 
-    return {{l, r}, true};
+    return {{l, r}};
 }
 
 int FindAnswer(std::vector<int> numbers, int target) {
@@ -45,9 +45,8 @@ int FindAnswer(std::vector<int> numbers, int target) {
     for (int i = 0; i < numbers.size() - 2; i++) {
         int value_here = numbers.at(i);
         auto maybe_pair = IndicesWithSumInSubrange(numbers, kTarget - value_here, i + 1, numbers.size() - 1);
-        if (maybe_pair.second) {
-            int l = maybe_pair.first.first;
-            int r = maybe_pair.first.second;
+        if (maybe_pair.has_value()) {
+            auto [l, r] = *maybe_pair;
             return value_here * numbers.at(l) * numbers.at(r);
         }
     }
